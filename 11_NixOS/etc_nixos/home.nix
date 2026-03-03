1f1d23a2bd172034e7753dc41edadf2510a2c893
Global Young <67513038+YoungHaKim7@users.noreply.github.com>
2026-03-04 01:26:12 +0900

    Create home.nix

---
+++ 11_NixOS/etc_nixos/home.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "gy";
+  home.homeDirectory = "/home/gy";
+  home.stateVersion = "25.11";
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nixos btw";
+      nrs = "sudo nixos-rebuild switch";
+    };
+
+    initExtra = ''
+      export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
+    '';
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.opacity = 0.9;
+      font.normal = {
+        family = "Hack Nerd Font";
+        style = "Regular";
+      };
+      font.size = 16;
+    };
+  };
+
+  home.file.".config/bat/config".text = ''
+    --theme="Nord"
+    --style="numbers,changes,grid"
+    --paging=auto
+  '';
+
+  #home.file.".config/qtile".source = /home/tony/home-manager-dotfiles/qtile;
+
+  home.packages = with pkgs; [
+    bat
+    neofetch
+  ];
+}
